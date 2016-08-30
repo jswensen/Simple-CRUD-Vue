a8c1298129b989f4c6ebb7acaf6cdd0326b0f25b
Joel Swensen <joel.swensen@gmail.com>
2016-08-30 15:59:41 -0700

    new libraries and some inputs

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+const app = express()
 
-const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, () => { console.log('listening on port 3000')});
+app.listen(3000, () => { console.log('listening on port 3000')})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body);
 });
