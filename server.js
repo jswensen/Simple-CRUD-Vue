3dbbceb723b6584c1ce88135f7955be626da4bf9
Joel Swensen <joel.swensen@gmail.com>
2016-09-08 09:25:35 -0700

    basic post

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body);
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
 
 let db;
