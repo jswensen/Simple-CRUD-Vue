26e0bceaceb472e9c538490b3eb883385ed28725
Joel Swensen <joel.swensen@gmail.com>
2016-09-08 09:19:30 -0700

    some server setup

--- server.js
+++ server.js
@@ -5,12 +5,22 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(3000, () => { console.log('listening on port 3000')})
+// app.listen(3000, () => { console.log('listening on port 3000')})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
-});
+})
 
 app.post('/quotes', (req, res) => {
   console.log(req.body);
-});
+})
+
+let db;
+
+MongoClient.connect('mongodb://<dbuser>:<dbpassword>@ds019936.mlab.com:19936/movie-quotes', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
