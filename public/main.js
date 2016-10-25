8cb0ca9e061ec82812d0351b05f44082eab10d92
Joel Swensen <joel.swensen@gmail.com>
2016-10-24 21:43:26 -0700

    making progress with vue and hardcoded data

--- public/main.js
+++ public/main.js
@@ -1,41 +1,34 @@
 var testVue = new Vue({
-// We want to target the div with an id of 'events'
   el: '#app',
-  // Here we can register any values or collections that hold data
-  // for the application
   data : {
     header : 'TESTING THE FORGE',
     item: { name: '', quote: '' },
     items: []
   },
-  // Anything within the ready function will run when the application loads
-  ready: function() {
+  created: function() {
     this.fetchQuotes();
   },
 
-  // Methods we want to use in our application are registered here
   methods: {
     fetchQuotes: function() {
       let items = [
       {
-        id: 1,
-        name: 'Darth Vader',
-        description: 'You will die braver than most.'
+        name: 'Hudson',
+        quote: 'Why don\'t you put her in charge!?'
       },
       {
-        id: 2,
         name: 'Thor',
-        description: 'You humans are so petty... and tiny',
+        quote: 'You humans are so petty... and tiny',
       }];
-      this.$set('items', items);
+      this.items = items || []; //prefer this to $set
     },
     addQuote: function() {
-    if(this.item.name) {
-      this.itmes.push(this.item);
-      this.item = { name: '', quote: '' };
+      if(this.item.name) {
+        this.items.push(this.item);
+        this.item = { name: '', quote: '' };
+      }
     }
-  }
-  }
+  } //methods
 })
 
 
