b534e3e488ec268e85000b635f8c7372fc5fb1df
Joel Swensen <joel.swensen@gmail.com>
2017-01-06 20:31:43 -0500

    generalize as items

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,43 @@
 Vue.component('modal', {
-  // props: ['id', 'name', 'quote'],
+  ['id', 'name', 'title']
   template: '#modal-template'
 })
 
 var testVue = new Vue({
   el: '#app',
   data : {
-    header : 'Movie Quotes',
-    item: { id: '', name: '', quote: '' },
+    header : 'To Do List',
+    item: { id: '', name: '', title: '' },
     items: [],
     showModal: false
   },
   created: function() {
-    this.fetchQuotes();
+    this.fetchItems();
   },
 
   methods: {
-    fetchQuotes: function() {
-      this.$http.get('quotes')
+    fetchItems: function() {
+      this.$http.get('titles')
         .then((items) => {
           this.items = items.body || [];  //prefer this to $set
         }, (error) => {
           alert(error);
         });
     },
-    addQuote: function() {
-      this.$http.post('quotes', this.item)
+    addItem: function() {
+      this.$http.post('titles', this.item)
         .then((response) => {
           this.item.id = response.body.id;  //add generated ID
           this.items.push(this.item);
-          this.item = {id : '', name : '', quote : ''};
+          this.item = {id : '', name : '', title : ''};
         }, (error) => {
           console.log(error);
         });
     },
-    deleteQuote: function(index) {
+    deleteItem: function(index) {
       console.log(this.items[index].name);
-      if(confirm(`Are you sure you want to delete this quote at index ${index}?`)) {
-        this.$http.put('quotes/delete', this.items[index])
+      if(confirm(`Are you sure you want to delete this title at index ${index}?`)) {
+        this.$http.put('titles/delete', this.items[index])
           .then((response) => {
             console.log('success');
             this.items.splice(index,1);  //$remove
@@ -46,11 +46,11 @@ var testVue = new Vue({
           });
       }
     },
-    updateQuote: function() {
+    updateItem: function() {
       console.log('UPDATE AT: ');
       console.log(index);
-      if(confirm(`Are you sure you want to update this quote at index ${index}?`)) {
-        this.$http.put('quotes' + JSON.stringify({'name' : this.items[index].name}))
+      if(confirm(`Are you sure you want to update this title at index ${index}?`)) {
+        this.$http.put('titles' + JSON.stringify({'name' : this.items[index].name}))
           .then((response) => {
             console.log('update success');
 
